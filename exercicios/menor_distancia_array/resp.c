789ebb353bf9daebea104343d49a25d1bc493eca
Gabriel <gabrielcosta3536@gmail.com>
2024-02-14 22:39:30 -0300

    finalizado

--- exercicios/menor_distancia_array/resp.c
+++ exercicios/menor_distancia_array/resp.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-int distancia(int num1[10], int num2[10]) {
+int valores(int n1, int n2) {
+    int menor;
+
+    if (n1 - n2 < 0) {
+        menor = (n1 - n2) * -1;
+    } else {
+        menor = n1 - n2;
+    }
+    return menor;
+}
+
+void distancia(int num1[10], int num2[10]) {
     int n1, n2, menor;
 
-    for(int c=0; c < 10; c++) {
+    for (int c = 0; c < 10; c++) {
 
-        for (int i=0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
 
-            if((c == 0 && i == 0) || ((num1[c] - num2[i]) >= 0)) {
+            if ((c == 0 && i == 0)) {
+                menor = valores(num1[c], num2[i]);
 
-                if ((num1[c] - num2[i]) <= menor){
+                n2 = num2[i];
+                n1 = num1[c];
+            } else if (valores(num1[c], num2[i]) < menor) {
 
-                    menor = (num1[c] - num2[i]);
-
-                    n2 = num2[i];
-                    n1 = num1[c];
-                }
+                menor = valores(num1[c], num2[i]);
+                n2 = num2[i];
+                n1 = num1[c];
             }
         }
     }
-    printf("num1 %d num2 %d\n", n1, n2);
+    printf("A menor distância seria a combinação do número %d do array 1 com o número %d do array 2, que seria %d de distância.",n1, n2, menor);
 }
 
-
-void main() {
-    int num2 [10] = {-1, 5, -10, 7, 4, 5, 1, 9, 35, 3};
-    int num1 [10] = {26, 6, 5, 10, 2, 0, 30, 40, -2, 10};
+int main() {
+    int num2[10] = {-1, 5, -10, 7, 4, 5, 1, 9, 35, 3};
+    int num1[10] = {26, 6, 5, 10, 2, 0, 30, 40, -2, 10};
 
     distancia(num1, num2);
 }
